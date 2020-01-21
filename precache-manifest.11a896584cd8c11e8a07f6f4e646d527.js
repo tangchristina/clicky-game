b94f7213d793d187acfeadef5ec90e9a924244ab
Christina Tang <christinatangerine@hotmail.com>
2020-01-20 21:56:14 -0800

    Updates

---
+++ precache-manifest.11a896584cd8c11e8a07f6f4e646d527.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6d84a357a527659ce06ba4f92e394c6",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "874520bfa551a5d75927",
+    "url": "/clicky-game/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7fdb4e0161f769fea68a",
+    "url": "/clicky-game/static/js/2.3c81f7e4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicky-game/static/js/2.3c81f7e4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "874520bfa551a5d75927",
+    "url": "/clicky-game/static/js/main.0297908e.chunk.js"
+  },
+  {
+    "revision": "a444cfe878aa3a515eec",
+    "url": "/clicky-game/static/js/runtime-main.24c47d54.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clicky-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
