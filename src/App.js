a6a4edc3960a04b02f1d3ab96528ea98334cfec7
Christina Tang <christinatangerine@hotmail.com>
2020-01-21 20:42:29 -0800

    created initial files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Jumbotron from "../components/Jumbotron";
+import Wrapper from "../components/Wrapper";
+import Nav from "../components/Nav";
+
+
+// class App extends Component {
+//   state = {
+//     books: []
+//   };
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  return <Nav/>;
+
+
+
+
+            //  <Jumbotron>
+            // <h1>Clicky Game!</h1>
+            // <p>Click on an image to earn points, but don't click on any more than once!</p>
+            // <Jumbotron/>;
+
+    
+  }
+
+
 
 export default App;
