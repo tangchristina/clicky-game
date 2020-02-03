5c9fd52ebf7498360e916424756f9dfd3f8ec01f
Christina Tang <christinatangerine@hotmail.com>
2020-02-02 18:49:10 -0800

    deploying to gh pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import ReactDOM from 'react-dom';
 import Jumbotron from "./components/Jumbotron/Jumbotron";
 import Wrapper from "./components/Wrapper/Wrapper";
 import Nav from "./components/Nav/Nav";
